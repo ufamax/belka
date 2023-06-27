0e575da20d1892afe9fe10440be4baf3ef830837
Maksim Novozhilov <ufamax@gmail.com>
2023-06-28 01:49:20 +0500

    added file test1.R

---
+++ test1.R
@@ -0,0 +1,15 @@
+
+build_ziggurat <- function(n) {
+  if (n==1) {
+    m <- matrix(n,n,n)
+  } else {
+    m <- matrix(n,n*2-1,n*2-1)
+    for (i in n:1) {
+      m[c(i,nrow(m)-i+1), ] <- i
+      m[, c(i,ncol(m)-i+1)] <- i
+    }
+  }
+  return (m)
+}
+
+z <- build_ziggurat(4); z
